4c3f0eb038abb6c4148f3a6f49de8c4a3fb8aa69
rahulhattinagre <rahulhattinagre@gmail.com>
2025-10-11 23:23:12 +0530

    Add React frontend build and updated backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 dotenv.config();
 
@@ -52,6 +55,16 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
